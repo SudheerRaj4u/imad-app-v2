5cbba83fbd9ee2f9b24f65631af4070bb3da3a64
SudheerRaj4u <sudheerraj4u@gmail.com>
2017-02-21 23:43:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('/article-one',function(req ,res){
     res.send('article one is requested here');
 });
@@ -21,6 +17,10 @@ app.get('/article-2',function(req ,res){
 app.get('/article-3',function(req ,res){
     res.send('article -3 is requested here');
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
