1ca536efe9a3548c3f43afcb4ef48aff9c20b4ae
SudheerRaj4u <sudheerraj4u@gmail.com>
2017-02-21 23:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one',function(req ,res){
-    res.sendFile('article one is requested here');
+    res.send('article one is requested here');
 });
 app.get('/article-2',function(req ,res){
-    res.sendFile('article one is requested here');
+    res.send('article one is requested here');
 });
 app.get('/article-one',function(req ,res){
-    res.sendFile('article -3 is requested here');
+    res.send('article -3 is requested here');
 });
 
 app.get('/ui/madi.png', function (req, res) {
